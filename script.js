54bff89bc2fd4910ee811406c6307940be042718
Melissa Freese <melissafreese@gmx.com>
2024-02-07 13:01:41 -0800

    Create function with for loops and create elements for row and gridBox

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const button = document.querySelector(".resetButton");
 const container = document.querySelector(".container");
+
+const createGrid = amtOfGrids => {
+	for (let i = 0; i < amtOfGrids; i++);
+	{
+		const row = document.createElement("div");
+		row.classList.add("grid-row");
+
+		for (let j = 0; i < amtOfGrids; j++);
+		{
+			const gridBox = document.createElement("div");
+			row.appendChild(gridBox);
+		}
+	}
+};

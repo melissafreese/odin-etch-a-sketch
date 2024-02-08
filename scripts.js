4dceafdce98859d8f409ab5212229ee35cefe103
Melissa Freese <melissafreese@gmx.com>
2024-02-08 13:32:34 -0800

    Fix typo by adding () to the height and width styling for gridCell

--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ sketchArea.style.height = `${GRIDSIDE}px`;
 function createGridCells() {
 	for (let i = 0; i < rows * cols; i++) {
 		const gridCell = document.createElement("div");
-		gridCell.style.width = `${GRIDSIDE / cols - 2}px`;
-		gridCell.style.height = `${GRIDSIDE / rows - 2}px`;
+		gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
+		gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
 		gridCell.classList.add("cell");
 
 		sketchArea.appendChild(gridCell);

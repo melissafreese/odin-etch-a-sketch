08e9fc737044969b3b160757aa0ba378dfe8fdc7
Melissa Freese <melissafreese@gmx.com>
2024-02-08 13:22:12 -0800

    Fix typo; had width twice instead of height when establishing the gridCell style

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function createGridCells() {
 	for (let i = 0; i < rows * cols; i++) {
 		const gridCell = document.createElement("div");
 		gridCell.style.width = `${GRIDSIDE / cols - 2}px`;
-		gridCell.style.width = `${GRIDSIDE / rows - 2}px`;
+		gridCell.style.height = `${GRIDSIDE / rows - 2}px`;
 		gridCell.classList.add("cell");
 
 		sketchArea.appendChild(gridCell);

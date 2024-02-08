bcf5b6fafed553610d25832d00eea5d5cdbc497e
Melissa Freese <melissafreese@gmx.com>
2024-02-08 15:17:50 -0800

    Combine rows and cols variable into one variable named squaresPerSide

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 const GRIDSIDE = 600;
-let rows = 16;
-let cols = 16;
+let squaresPerSide = 16;
 
 const sketchArea = document.querySelector("#sketch-area");
 sketchArea.style.width = `${GRIDSIDE}px`;
